cf38f81f8ad85dd5107dbf4c52d95a408611d969
Brooke <46959407+nimajnebec@users.noreply.github.com>
2025-06-29 23:24:56 +0100

    fix: use polyfill instead of patch

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,9 +1,10 @@
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import { custom } from "./plugin";
 
 export default defineConfig({
-	plugins: [sveltekit(), custom()],
+	plugins: [sveltekit(), custom(), nodePolyfills({ include: ["path"] })],
 	server: {
 		proxy: {
 			"/api/": {
